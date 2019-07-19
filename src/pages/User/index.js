4707477bdba84a1c67939366c94ecb9fbd1bcb67
Montezi <mirianmontezi.pro@gmail.com>
2019-07-18 23:49:10 -0300

    First Commit

---
+++ src/pages/User/index.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import ShimmerPlaceHolder from 'react-native-shimmer-placeholder';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
+
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+    visible: false,
+  };
+
+  async componentDidMount() {
+    setTimeout(() => this.setState({ visible: !this.state.visible }), 2500);
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    const { navigation } = this.props;
+    const { stars } = this.state;
+    const user = navigation.getParam('user');
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <ShimmerPlaceHolder
+              style={{
+                flex: 1,
+                height: 50,
+                marginBottom: 20,
+                borderRadius: 4,
+                flexDirection: 'row',
+              }}
+              autoRun
+              visible={this.state.visible}
+            >
+              <Starred>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            </ShimmerPlaceHolder>
+          )}
+        />
+      </Container>
+    );
+  }
+}
