69a98b712ec378dd93b6314c3f654bd959e662e3
Montezi <mirianmontezi.pro@gmail.com>
2019-07-20 21:58:43 -0300

    update:onEndReached & shimmer

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
 import ShimmerPlaceHolder from 'react-native-shimmer-placeholder';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
@@ -31,23 +32,59 @@ export default class User extends Component {
   state = {
     stars: [],
     visible: false,
+    loading: false,
+    page: 1,
+    more: false,
   };
 
   async componentDidMount() {
     const { visible } = this.state;
     setTimeout(() => this.setState({ visible: !visible }), 2500);
-    const { navigation } = this.props;
-    const user = navigation.getParam('user');
-
-    const response = await api.get(`/users/${user.login}/starred`);
-
-    this.setState({ stars: response.data });
+    this.loadStars();
   }
+
+  loadStars = async (page = 1) => {
+    // const { loading } = this.state;
+    // if (loading) return;
+    const { navigation } = this.props;
+    const { stars } = this.state;
+    const user = navigation.getParam('user');
+    // const { page } = this.state;
+
+    // this.setState({ loading: true });
+
+    const response = await api.get(`/users/${user.login}/starred?page=${page}`);
+
+    this.setState({
+      stars: [...stars, ...response.data],
+      page,
+      loading: false,
+      more: response.headers.link && response.headers.link.includes('next'),
+    });
+  };
+
+  loadMore = () => {
+    const { page, more } = this.state;
+
+    if (more) {
+      this.setState({ loading: true });
+      this.loadStars(page + 1);
+    }
+  };
+
+  renderFooter = () => {
+    const { loading } = this.state;
+    if (!loading) return null;
+    return (
+      <View style={styles.loading}>
+        <ActivityIndicator />
+      </View>
+    );
+  };
 
   render() {
     const { navigation } = this.props;
-    const { stars } = this.state;
-    const { visible } = this.state;
+    const { stars, visible } = this.state;
     const user = navigation.getParam('user');
 
     return (
@@ -60,29 +97,42 @@ export default class User extends Component {
         <Stars
           data={stars}
           keyExtractor={star => String(star.id)}
+          onEndReached={this.loadMore}
+          onEndReachedThreshold={0.2}
+          ListFooterComponent={this.renderFooter}
           renderItem={({ item }) => (
-            <ShimmerPlaceHolder
-              style={{
-                flex: 1,
-                height: 50,
-                marginBottom: 20,
-                borderRadius: 4,
-                flexDirection: 'row',
-              }}
-              autoRun
-              visible={visible}
-            >
-              <Starred>
+            <Starred>
+              <ShimmerPlaceHolder
+                style={styles.avatarOwner}
+                autoRun
+                visible={visible}
+              >
                 <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
-                <Info>
+              </ShimmerPlaceHolder>
+              <Info>
+                <ShimmerPlaceHolder autoRun visible={visible}>
                   <Title>{item.name}</Title>
+                </ShimmerPlaceHolder>
+                <ShimmerPlaceHolder autoRun visible={visible}>
                   <Author>{item.owner.login}</Author>
-                </Info>
-              </Starred>
-            </ShimmerPlaceHolder>
+                </ShimmerPlaceHolder>
+              </Info>
+            </Starred>
           )}
         />
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  avatarOwner: {
+    height: 42,
+    width: 42,
+    borderRadius: 21,
+  },
+  loading: {
+    alignSelf: 'center',
+    marginVertical: 20,
+  },
+});
