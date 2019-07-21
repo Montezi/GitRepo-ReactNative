5727f3d45eeadbda5ad9f88d79a65a5e013676c9
Montezi <mirianmontezi.pro@gmail.com>
2019-07-21 17:40:37 -0300

    chore: update criar WebView

--- android/app/src/main/java/com/modulo06/MainApplication.java
+++ android/app/src/main/java/com/modulo06/MainApplication.java
@@ -3,6 +3,7 @@ package com.modulo06;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCWebViewPackage(),
             new LinearGradientPackage(),
             new AsyncStoragePackage(),
             new VectorIconsPackage(),
